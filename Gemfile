3d3ba07d9793fb733b921dec3e41d4a92246df5e
Jean-Michel Garnier <jean-michel@21croissants.com>
2012-09-20 15:14:23 +0200

    21croissants custom project template. Thanks Michael Grosser!

--- Gemfile
+++ Gemfile
@@ -2,4 +2,6 @@ source :rubygems
 gemspec
 
 gem "rake"
-gem "rspec", "~>2"
+gem "rspec", "~> 2.11"
+gem "guard-rspec"
+gem "rb-fsevent", "~> 0.9.1"
--- copy.rb
+++ copy.rb
@@ -1,8 +1,18 @@
 #!/usr/bin/env ruby
 require 'rubygems'
 require 'rake'
-require 'active_support/core_ext/string'
 require 'yaml'
+
+# activesupport/lib/active_support/inflector/methods.rb, line 48
+  def underscore(camel_cased_word)
+    word = camel_cased_word.to_s.dup
+    word.gsub!(/::/, '/')
+    word.gsub!(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+    word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
+    word.tr!("-", "_")
+    word.downcase!
+    word
+  end
 
 # extract options
 gem_class_name = ARGV[0]
@@ -10,7 +20,7 @@ gem_description = ARGV[1]
 if not gem_class_name or not gem_description
   abort "Usage: ./template/copy.rb MyNewLib 'A great lib to do stuff'"
 end
-gem_name = gem_class_name.underscore
+gem_name = underscore gem_class_name
 
 # load settings
 here = File.dirname(__FILE__)
@@ -18,12 +28,12 @@ SETTINGS = YAML.load_file("#{here}/.copy.yml")
 
 # copy files
 files_to_copy = Dir["#{here}/**/**"] + ["#{here}/.rvmrc", "#{here}/.travis.yml"] - ["#{here}/copy.rb"]
-files_to_copy.reject!{|f| File.directory?(f) }
+files_to_copy.reject! { |f| File.directory?(f) }
 
 files_to_copy.each do |file_to_copy|
   # make new dir and file
   new_file = file_to_copy
-  new_file = new_file.sub(here,'').sub(%r{^/},'').sub('GEM_NAME', gem_name)
+  new_file = new_file.sub(here, '').sub(%r{^/}, '').sub('GEM_NAME', gem_name)
   new_file = "#{gem_name}/#{new_file}"
   sh "mkdir -p #{File.dirname(new_file)} 2>&1"
 
@@ -36,14 +46,12 @@ files_to_copy.each do |file_to_copy|
   content.gsub!('GEM_NAME', gem_name)
   content.gsub!('GEM_CLASS_NAME', gem_class_name)
   content.gsub!('GEM_DESCRIPTION', gem_description)
-  File.open(new_file, 'w'){|f| f.write content }
+  File.open(new_file, 'w') { |f| f.write content }
 end
-
-# check tests and (fast) generate Gemfile.lock
-sh "cd #{gem_name} && bundle exec rake"
 
 # commit everything into 'initial'
 sh "cd #{gem_name} && git init && git add . && git commit -m 'initial by http://github.com/grosser/project_template'"
 
 puts "#{gem_name} is now ready at ./#{gem_name}"
+
 
